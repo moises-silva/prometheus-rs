782074f1063db57608c39a4fbfad82f459d20643
Moises Silva <moises.silva@gmail.com>
2016-07-31 12:46:59 -0400

    Add counter and registry structs
    Still a clunky as I fight with the borrow checker, but does compile.
    
    The counter reference seems to be keeping an implicit mutable reference
    to the registry so this won't be usable. I need to move the registry
    to keep a vector of reference to counters as we won't be the owners
    of the counters really, the application will be.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,51 @@
 extern crate tiny_http;
 use tiny_http::{Server, Response};
 
-pub fn handler() {
-    println!("Handling metrics request!");
-    let server = Server::http("0.0.0.0:6780").unwrap();
-    loop {
-        let request = match server.recv() {
-            Ok(rq) => rq,
-            Err(e) => { println!("error: {}", e); break }
-        };
-        let response = Response::from_string("Prometheus Metrics".to_string());
-        let _ = request.respond(response);
+pub struct Counter {
+    name: String,
+    desc: String,
+    value: i64
+}
+
+impl Counter {
+    pub fn increment(&mut self) -> i64 {
+        self.value + 1
     }
 }
 
+pub struct Registry {
+    counters: Vec<Counter>
+}
+
+impl Registry {
+    pub fn new() -> Registry {
+        Registry {
+            counters: Vec::new()
+        }
+    }
+
+    pub fn counter(&mut self, name: String, desc: String) -> &mut Counter {
+        let c = Counter {
+            name: name,
+            desc: desc,
+            value: 0
+        };
+        self.counters.push(c);
+        self.counters.last_mut().unwrap()
+    }
+
+    pub fn start(&self) {
+        println!("Startings metrics http endpoint");
+        let server = Server::http("0.0.0.0:6780").unwrap();
+        loop {
+            let request = match server.recv() {
+                Ok(rq) => rq,
+                Err(e) => { println!("error: {}", e); break }
+            };
+            println!("Handling metrics request");
+            let response = Response::from_string("Prometheus Metrics".to_string());
+            let _ = request.respond(response);
+        }
+    }
+}
 
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ extern crate prometheus;
 
 fn main() {
     println!("Hello, Prometheus!");
-    prometheus::handler();
+    let mut reg = prometheus::Registry::new();
+    {
+        let counter = reg.counter("http_requests".to_string(), "Counter for HTTP Requests".to_string());
+        counter.increment();
+    }
+    reg.start()
 }
